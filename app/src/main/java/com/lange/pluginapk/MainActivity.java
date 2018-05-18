41470a2019b772f4a0a5e26b13de065bc45ce5f4
YY_Duan <lange@anybeen.net>
2018-05-18 16:13:01 +0800

    commit can use version

--- app/src/main/java/com/lange/pluginapk/MainActivity.java
+++ app/src/main/java/com/lange/pluginapk/MainActivity.java
@@ -1,13 +1,15 @@
 package com.lange.pluginapk;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        Testjni testjni = new Testjni();
+//        Toast.makeText(this,testjni.getString(),Toast.LENGTH_SHORT).show();
     }
 }
